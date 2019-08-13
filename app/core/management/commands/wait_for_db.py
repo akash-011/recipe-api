81534179866617fe7dff762c14c81eaf9ca61014
Akash <akashbaga011@gmail.com>
2019-08-13 18:12:37 +0300

    Fix migrations issue

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,16 +6,17 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database"""
+    """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write("Waiting for Database...")
+        """Handle the command"""
+        self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections["default"]
             except OperationalError:
-                self.stdout.write("Database not available, waiting 1 second ... ")
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS("Database Connected"))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
